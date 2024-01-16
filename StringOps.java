699c2faa9ede9724d00248d4c6ffdec9f47f68e3
Yuval Ben-Zvi <yuval.benzvi@post.runi.ac.il>
2024-01-16 23:07:29 +0200

    hw4

--- StringOps.java
+++ StringOps.java
@@ -22,21 +22,89 @@ public class StringOps {
     //////                                               ///////
     ////////////////////////////////////////////////////////////
     public static void main(String[] args) {
-        
+    String string = args[0];
+    //System.out.println(string);
+    //System.out.println(capVowelsLowRest(string));
+    //System.out.println(camelCase(string));
+    char chr = 'L';
+    for (int i = 0; i < allIndexOf(string, chr).length; i++) {
+        System.out.print(allIndexOf(string, chr)[i] + " ");
+        }
     }
 
     public static String capVowelsLowRest (String string) {
-        // Write your code here:
-        return "";
+        int n = string.length();
+        String s = "";
+        for (int i = 0; i < n; i++) {
+            if (string.charAt(i) == 32) {
+                s = s + " ";
+            } else {
+            if (string.charAt(i) == 'a' || string.charAt(i) == 'e' || string.charAt(i) == 'o' || string.charAt(i) == 'u' || string.charAt(i) == 'i') {
+                s = s + (char) (string.charAt(i) - 32);
+            } else {
+            if (string.charAt(i) == 'A' || string.charAt(i) == 'E' || string.charAt(i) == 'O' || string.charAt(i) == 'U' || string.charAt(i) == 'I') {
+                s = s + string.charAt(i);
+            } else {
+            if (string.charAt(i) >= 65 && string.charAt(i) <= 90) {
+                s = s + (char) (string.charAt(i) + 32);
+            } else {
+                s = s + string.charAt(i);
+            }}}}
+        }
+        return s;
     }
 
     public static String camelCase (String string) {
-        // Write your code here:
-        return "";
+        //System.out.print("check 1 :");
+        int n = string.length();
+        String s = "";
+        int i = 0;
+        while (string.charAt(i) == 32) {
+            i++;
+        }
+        //System.out.print("check 2 :" + s);
+        while (i < n && string.charAt(i) != 32) {
+            if (string.charAt(i) >= 65 && string.charAt(i) <= 90) {
+                s = s + (char) (string.charAt(i) + 32);
+            } else {
+                s = s + string.charAt(i);
+            }
+            i++;
+        }
+        //System.out.print("check 3 :" + s);
+        while (i < n) {
+            if (string.charAt(i-1) == 32) {
+                if (string.charAt(i) >= 65 && string.charAt(i) <= 90) {
+                    s = s + string.charAt(i);
+                } else {
+                    s = s + (char) (string.charAt(i) - 32);
+                }
+            }
+            if (string.charAt(i) != 32 && string.charAt(i-1) != 32 && string.charAt(i) >= 65 && string.charAt(i) <= 90) {
+                s = s + (char) (string.charAt(i) + 32);
+            } else {
+                if (string.charAt(i) != 32 && string.charAt(i-1) != 32) {
+                s = s + string.charAt(i);
+            }}
+            i++;
+        }
+        return s;
     }
 
     public static int[] allIndexOf (String string, char chr) {
-        // Write your code here:
-        return new int[1];
+        int n = 0; // new array length 
+        int j = 0; // new array index
+        for (int i = 0; i < string.length(); i++) {
+            if (string.charAt(i) == chr)
+                n++;
+        }
+        int[] arr = new int [n];
+        for (int i = 0; i < string.length(); i++) {
+            if (string.charAt(i) == chr) {
+                arr[j] = i;
+                j++;   
+            }
+        }
+        return arr;
     }
 }
